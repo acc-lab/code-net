e84cdf6f66a7d6a00bcac2ee869a0fa24979d5d7
Acclab <63785690+acc-lab@users.noreply.github.com>
2020-05-21 11:24:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var textbox = document.getElementById("textbox");
 
+var row_text = document.getElementById("row-text");
+
 function update(){
-	//textbox.value
+	row_text_inner = "";
+	
+	first_line = Math.round(textbox.scrollTop/15)
+	
+	for(i=1;i<=19;i++){
+		row_text_inner+=first_line+i+"\n";
+	}
+	
+	row_text.innerHTML=row_text_inner;
 }
 
-setInterval(update, 30);
+setInterval(update, 1);
